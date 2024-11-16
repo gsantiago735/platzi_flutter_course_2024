8218fc85114f08c583994ba830d08e338bcebba5
gsantiago735 <gsantiago202111@gmail.com>
2024-11-16 14:09:00 -0400

    Chapter 6

--- lib/main.dart
+++ lib/main.dart
@@ -25,10 +25,43 @@ class RecipeBook extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.orange,
       appBar: AppBar(
-          title: const Text(
-        "Recipe Book",
-        style: TextStyle(color: Colors.white),
-      )),
+        title: const Text(
+          "Recipe Book",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Container(
+        height: 125,
+        width: MediaQuery.of(context).size.width,
+        child: Card(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: 125,
+                width: 100,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  child: Container(),
+                ),
+              ),
+              const SizedBox(width: 26),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text("Comida"),
+                  const Text("Autor Nombre"),
+                  Container(
+                    height: 2,
+                    width: 75,
+                    color: Colors.orange,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
